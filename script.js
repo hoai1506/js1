c479e7f61fee9c9ed3d9d1c10939084723534e95
hoai1506 <luuhoai3107@gmail.com>
2023-01-06 21:07:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function hex2bin(hex){
+    return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-7);
+}
+
+console.log(hex2bin('FF'));
+
+function hex2bin(hex){
+    return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-7);
+}
+
+console.log(hex2bin('AA'));
+
+function hex2bin(hex){
+    return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-7);
+}
+
+console.log(hex2bin('AF'));
+
+
+
+
+
